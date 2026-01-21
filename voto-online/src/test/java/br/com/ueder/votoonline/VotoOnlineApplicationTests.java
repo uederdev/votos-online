63e70626c7f50d4b390a8b09d8b98afad5af6362
Ueder Carlos Costa Caetano <uedercosta@gmail.com>
2026-01-21 15:54:35 -0300

    Criação da Api - Voto On-line

---
+++ voto-online/src/test/java/br/com/ueder/votoonline/VotoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ueder.votoonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
